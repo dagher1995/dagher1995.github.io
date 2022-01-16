72ff15de8c42affb13e2bd65aed17ca986dc1023
ignore-crypto <33722184+dagher1995@users.noreply.github.com>
2022-01-16 15:09:15 +0400

    update

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ let ETH_VALUE = document.getElementById('eth-value');
 let XRP_VALUE = document.getElementById('xrp-value');
 let BNB_VALUE = document.getElementById('bnb-value');
 
+let BTC_VALUE_CONTAINER = document.getElementById('btc-value-container');
+let ETH_VALUE_CONTAINER = document.getElementById('eth-value-container');
+let XRP_VALUE_CONTAINER = document.getElementById('xrp-value-container');
+let BNB_VALUE_CONTAINER = document.getElementById('bnb-value-container');
+
 const btc_url='https://api3.binance.com/api/v3/ticker/price?symbol=BTCUSDT';
 const eth_url='https://api3.binance.com/api/v3/ticker/price?symbol=ETHUSDT';
 const xrp_url='https://api3.binance.com/api/v3/ticker/price?symbol=XRPUSDT';
@@ -27,6 +32,12 @@ let btc_owned = 0.0;
 let eth_owned = 0.0395604;
 let xrp_owned = 165.0;
 let bnb_owned = 0.00121477;
+
+// values in OMR converted to usd
+let btc_buy_value = 0.0 / 0.385;
+let eth_buy_value = 50.0 / 0.385; 
+let xrp_buy_value = 50.0 / 0.385;
+let bnb_buy_value = 0.5;
 
 BTC_OWNED.innerHTML = "لدينا: " + btc_owned;
 ETH_OWNED.innerHTML = "لدينا: " + eth_owned;
@@ -59,7 +70,13 @@ function getCoinPrice(url, node, number) {
                 BTC_PRICE.style.color = "red";
             } 
             node.innerHTML = "$ " + n;
-            BTC_VALUE.innerHTML = "$ " + (n * btc_owned).toFixed(2);
+            let value = (n * btc_owned).toFixed(2)
+            if(value > btc_buy_value) {
+                BTC_VALUE_CONTAINER.style.backgroundColor = "green";
+            } else {
+                BTC_VALUE_CONTAINER.style.backgroundColor = "red";
+            }
+            BTC_VALUE.innerHTML = "$ " + value;
             btc_last_price = n;
         } else if(number === 2) {
             if(n > eth_last_price) {
@@ -68,7 +85,13 @@ function getCoinPrice(url, node, number) {
                 ETH_PRICE.style.color = "red";
             } 
             node.innerHTML = "$ " + n;
-            ETH_VALUE.innerHTML = "$ " + (n * eth_owned).toFixed(2);
+            let value = (n * eth_owned).toFixed(2)
+            if(value > eth_buy_value) {
+                ETH_VALUE_CONTAINER.style.backgroundColor = "green";
+            } else {
+                ETH_VALUE_CONTAINER.style.backgroundColor = "red";
+            }
+            ETH_VALUE.innerHTML = "$ " + value;
             eth_last_price = n;
         } else if(number === 3) {
             if(n > xrp_last_price) {
@@ -77,7 +100,13 @@ function getCoinPrice(url, node, number) {
                 XRP_PRICE.style.color = "red";
             } 
             node.innerHTML = "$ " + n;
-            XRP_VALUE.innerHTML = "$ " + (n * xrp_owned).toFixed(2);
+            let value = (n * xrp_owned).toFixed(2)
+            if(value > xrp_buy_value) {
+                XRP_VALUE_CONTAINER.style.backgroundColor = "green";
+            } else {
+                XRP_VALUE_CONTAINER.style.backgroundColor = "red";
+            }
+            XRP_VALUE.innerHTML = "$ " + value;
             xrp_last_price = n;
         } else if(number === 4) {
             if(n > bnb_last_price) {
@@ -86,15 +115,21 @@ function getCoinPrice(url, node, number) {
                 BNB_PRICE.style.color = "red";
             } 
             node.innerHTML = "$ " + n;
-            BNB_VALUE.innerHTML = "$ " + (n * bnb_owned).toFixed(2);
+            let value = (n * bnb_owned).toFixed(2)
+            if(value > bnb_buy_value) {
+                BNB_VALUE_CONTAINER.style.backgroundColor = "green";
+            } else {
+                BNB_VALUE_CONTAINER.style.backgroundColor = "red";
+            }
+            BNB_VALUE.innerHTML = "$ " + value;
             bnb_last_price = n;
         }
     }
 }
 
 const GetPrices = async () => {
-    // let i = 0;
-    while (true) {
+    let i = 0;
+    while (i < 5) {
         getCoinPrice(btc_url, BTC_PRICE, 1);
         await delay(2000);
         getCoinPrice(eth_url, ETH_PRICE, 2);
@@ -104,7 +139,7 @@ const GetPrices = async () => {
         getCoinPrice(bnb_url, BNB_PRICE, 4);
         await delay(2000);
 
-        // i++;
+        i++;
     }
   };
 
